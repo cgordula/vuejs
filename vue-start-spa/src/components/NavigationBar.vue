881d588973adad4a9861c6b7edcb65d92bd51fca
cgordula <carminagordula@gmail.com>
2023-06-04 17:49:09 +0700

    starting the project, vue create vue-create-spa, vue serve to run

---
+++ vue-start-spa/src/components/NavigationBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav
+    :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Vue</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li v-for="(page, index) in pages" :key="index" class="nav-item">
+            <a
+              class="nav-link"
+              :class="{ active: activePage === index }"
+              :href="page.link.url"
+              :title="`This link goes to the ${page.link.text} page`"
+              @click.prevent="navLinkClick(index)"
+              >{{ page.link.text }}</a
+            >
+          </li>
+        </ul>
+        <form class="d-flex">
+          <button
+            class="btn btn-outline-success"
+            @click.prevent="changeTheme()"
+          >
+            On/Off
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['pages', 'activePage', 'navLinkClick'],
+  data() {
+    return {
+      theme: 'light',
+    };
+  },
+  methods: {
+    changeTheme() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light';
+    },
+  },
+};
+</script>
