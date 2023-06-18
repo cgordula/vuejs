19dc40c3ca0b24ffcf9cc43e6ba1b82d86fbebc0
cgordula <carminagordula@gmail.com>
2023-06-18 12:00:26 +0700

    form, input, v-model vs. :value and @input directives

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -11,17 +11,23 @@
 
   <!-- v-if does not output element at all if it is false and it is simple approach when using props-->
   <!-- <page-viewer v-if="pages.length > 0" :page="pages[activePage]"> </page-viewer> -->
-  <page-viewer :page="pages[activePage]"> </page-viewer>
+
+  <!-- <page-viewer :page="pages[activePage]"> </page-viewer> -->
+
+  <create-page :page-created="pageCreated"> </create-page>
 </template>
 
 <script>
+/* eslint-disable */
 import NavigationBar from './components/NavigationBar.vue';
 import PageViewer from './components/PageViewer.vue';
+import CreatePage from './components/CreatePage.vue';
 
 export default {
   components: {
     NavigationBar,
     PageViewer,
+    CreatePage,
   },
   created() {
     this.getPages();
@@ -38,6 +44,9 @@ export default {
       let data = await res.json();
 
       this.pages = data;
+    },
+    pageCreated(pageObj) {
+      console.log(pageObj);
     },
   },
 };
